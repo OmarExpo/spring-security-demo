f5a6a1aae5424a80878da1e5d9053aea2f310458
omar <omar1807@stud.kea.dk>
2021-10-03 22:48:21 +0200

    Adding 'ant_matchers' patterns for whitelisting

--- src/main/java/springsecurity/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/springsecurity/demo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,16 @@
 package springsecurity.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +20,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             Below overridden method means:
               _ any request must be authenticated by using httpBasic mechanism
      */
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,5 +39,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser =  User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser
+        );
     }
 }
